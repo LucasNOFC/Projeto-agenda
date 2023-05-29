bbb84a95f214a205c0ba9f70cb689afe9f97c199
NobreREAL <79422432+NobreREAL@users.noreply.github.com>
2023-05-29 19:23:06 -0300

    Correções

--- frontend/main.js
+++ frontend/main.js
@@ -2,3 +2,9 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import './assets/css/style.css';
+
+import Login from './modules/Login';
+
+const login = new Login('.form-login');
+
+login.init();
